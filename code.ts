243c4dde509de34062cabb8cb553ae0d22886889
thuno <thuno2698@gmail.com>
2024-10-22 17:13:07 +0700

    wini-icons init figma plugin

---
+++ code.ts
@@ -0,0 +1,31 @@
+// Show a simple UI to upload an SVG file
+figma.showUI(`
+  <html>
+    <input type="file" id="upload" accept=".svg" />
+    <script>
+      document.getElementById('upload').onchange = function(event) {
+        const file = event.target.files[0];
+        const reader = new FileReader();
+        reader.onload = function(e) {
+          const svgData = e.target.result;
+          parent.postMessage({ pluginMessage: { type: 'import-svg', svgData } }, '*');
+        };
+        reader.readAsText(file);
+      };
+    </script>
+  </html>
+`, { width: 300, height: 200 });
+
+figma.ui.onmessage = (msg) => {
+  if (msg.type === 'import-svg') {
+    const svgNode = figma.createNodeFromSvg(msg.svgData);
+    figma.currentPage.appendChild(svgNode);
+
+    // Optionally adjust position or other properties of the imported SVG
+    svgNode.x = 100;
+    svgNode.y = 100;
+
+    figma.viewport.scrollAndZoomIntoView([svgNode]);
+    figma.closePlugin();
+  }
+};
